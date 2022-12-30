4994efae463518ecefe61cb488e018f9793e5ea9
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2022-12-30 17:39:12 +0900

    Change type typescript

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "stories": [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  "webpackFinal": async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    })
+
+    return config
+  },
+}
