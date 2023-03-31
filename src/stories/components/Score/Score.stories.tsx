d29e7dee5103741a52a5577e55b82dce71fc7fe2
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-03-31 19:22:43 +0900

    fix: score style

---
+++ src/stories/components/Score/Score.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Score } from './Score';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Component/Score',
+  component: Score,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof Score>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Score> = (args) => <Score {...args} />;
+
+export const DisplayScore = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+DisplayScore.args = {
+  score: '3',
+};
---
+++ src/stories/components/Score/Score.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '@/scss/object/component/_score.scss';
+import '@/scss/object/utility/index.scss';
+
+type TScoreProps = {
+  score: '1' | '2' | '3' | '4' | '5';
+};
+
+export const Score = ({ score }: TScoreProps) => {
+  return (
+    <span className="c-score">
+      <span className="c-score__count">{score}</span>
+    </span>
+  );
+};
