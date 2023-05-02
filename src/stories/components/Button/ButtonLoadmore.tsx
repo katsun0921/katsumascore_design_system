7eaae7ca1d7d866071bd8cd96bf614186e889c96
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-05-02 13:42:12 +0900

    fix: mansonry styles

---
+++ src/stories/components/Button/ButtonLoadmore.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '@/scss/object/component/_button.scss';
+
+export const ButtonLoadmore = () => (
+  <div className='c-button__loadmore'>
+    <a href='#'>Load More</a>
+  </div>
+);
