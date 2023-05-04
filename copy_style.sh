fa1607112309309ebee96c249b23b8056e8d7aba
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-05-05 08:23:42 +0900

    fix: slider

--- copy_style.sh
+++ copy_style.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-echo "ビルドしたsytleファイルのコピーを開始"
+echo "ビルドしたファイルのコピーを開始"
 
 PATH_KATSUMASCORE="/c/Users/katsu/Local Sites/katsumascore/app/public/wp-content/themes/katsumascore"
 
-echo "style.cssとbundle.jsを削除"
+echo "all.cssとbundle.jsを削除"
 
-rm "$PATH_KATSUMASCORE"/style.css
+rm "$PATH_KATSUMASCORE"/css/all.css
 rm "$PATH_KATSUMASCORE"/js/bundle.js
 
 echo "古いsytleファイルとビルドしたsytle.cssをマージ"
 
 # Set the input file names and the output file name
 build_style="./dist/build.css"
-new_style="./dist/style.css"
+new_style="./dist/all.css"
 
 # Use the cat command to merge the files and save the result to the output file
 cat "$build_style"  > "$new_style"
 
-echo "ビルドしたsytle.cssをコピー"
-cp ./dist/style.css "$PATH_KATSUMASCORE"
+echo "ビルドしたファイルをkatsumascore theme へコピー"
+cp ./dist/all.css "$PATH_KATSUMASCORE/css"
 cp ./dist/bundle.js "$PATH_KATSUMASCORE/js"
